4e04fbff1e1423818dd0b5cd38be464de5665231
Tomás Aranda <tomas.aranda.95@gmail.com>
2022-05-18 14:14:22 -0300

    feat: add database seeder with worker

--- app/src/main/java/com/example/poke_wordle/MainActivity.kt
+++ app/src/main/java/com/example/poke_wordle/MainActivity.kt
@@ -1,18 +1,8 @@
 package com.example.poke_wordle
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.poke_wordle.databinding.ActivityMainBinding
-import com.example.poke_wordle.db.AppDatabase
-import com.example.poke_wordle.db.Pokemon
-import com.example.poke_wordle.network.PokemonList
-import com.example.poke_wordle.network.PokemonService
-import kotlinx.coroutines.CoroutineExceptionHandler
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -21,51 +11,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        // TODO("SEED DB ONCREATE ROOM")
-        // getPokemonList()
-    }
-
-    private val coroutineExceptionHandler = CoroutineExceptionHandler{ _, throwable ->
-        Log.e("EXCEPTION HANDLER", throwable.cause.toString())
-        throwable.message?.let { Log.e("EXCEPTION HANDLER", it) }
-        throwable.printStackTrace()
-    }
-
-    private fun getPokemonList() {
-        CoroutineScope(Dispatchers.IO + coroutineExceptionHandler).launch {
-            val call = PokemonService.create().getPokemonList()
-            val pokemonList = call.body()?.toEntityList()
-            if (call.isSuccessful) {
-                if (pokemonList != null) {
-                    AppDatabase.getInstance(applicationContext).pokemonDao().insertAll(pokemonList)
-                }
-                runOnUiThread {
-                    showSuccess()
-                }
-            } else {
-                runOnUiThread {
-                    showError()
-                }
-            }
-        }
-    }
-
-    private fun showError() {
-        Toast.makeText(this,"Error al buscar pokemones",Toast.LENGTH_SHORT).show()
-    }
-
-    private fun showSuccess() {
-        Toast.makeText(this,"Se agregaron Los pokemons a DB",Toast.LENGTH_SHORT).show()
-    }
-
-}
-
-private fun PokemonList.toEntityList(): List<Pokemon> {
-    return this.results.map {
-        Pokemon(
-            it.url.split('/').dropLast(1).last().toInt(),
-            it.name,
-            it.url
-        )
     }
 }
--- app/src/main/java/com/example/poke_wordle/db/AppDatabase.kt
+++ app/src/main/java/com/example/poke_wordle/db/AppDatabase.kt
@@ -5,6 +5,10 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.sqlite.db.SupportSQLiteDatabase
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import com.example.poke_wordle.workers.SeedDatabaseWorker
 
 @Database(
     version = 1,
@@ -26,7 +30,18 @@ abstract class AppDatabase : RoomDatabase() {
         }
 
         private fun buildDatabase(context: Context): AppDatabase {
-            return Room.databaseBuilder(context, AppDatabase::class.java, "poke-wordle-db").build()
+            return Room.databaseBuilder(context, AppDatabase::class.java, "poke-wordle-db")
+                .addCallback(
+                    object : RoomDatabase.Callback() {
+                        override fun onCreate(db: SupportSQLiteDatabase) {
+                            super.onCreate(db)
+                            val request = OneTimeWorkRequestBuilder<SeedDatabaseWorker>()
+                                .build()
+                            WorkManager.getInstance(context).enqueue(request)
+                        }
+                    }
+                )
+                .build()
         }
     }
 }
