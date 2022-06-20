d27a6bee25c3b10b5efe25961679c42682287999
Tomás Aranda <tomas.aranda.95@gmail.com>
2022-06-20 20:52:04 -0300

    feat: add programmatically wordle letter views

--- app/src/main/java/com/example/poke_wordle/GameFragment.kt
+++ app/src/main/java/com/example/poke_wordle/GameFragment.kt
@@ -1,14 +1,19 @@
 package com.example.poke_wordle
 
+import android.graphics.Typeface
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.example.poke_wordle.databinding.FragmentGameBinding
 import com.example.poke_wordle.db.AppDatabase
+import com.example.poke_wordle.domain.PokeWordle
 import com.example.poke_wordle.network.PokemonService
 import com.example.poke_wordle.repository.PokeWordlePlayRepository
 import com.example.poke_wordle.repository.PokemonRepository
@@ -37,17 +42,10 @@ class GameFragment : Fragment() {
     ): View {
         binding = FragmentGameBinding.inflate(inflater)
 
-        wordleViewModel.wordle.observe(viewLifecycleOwner) {
-            if (it != null) {
-                binding.attempts.text = it.attempts.toString()
+        wordleViewModel.wordle.observe(viewLifecycleOwner) { wordle ->
+            if (wordle != null) {
+                addWordleLetterViews(wordle)
             }
-        }
-
-        binding.attempts.setOnClickListener {
-            wordleViewModel.addLetter('p')
-        }
-        binding.difficultyLevel.setOnClickListener {
-            wordleViewModel.removeLastLetter()
         }
 
         val levelsArray = resources.getStringArray(R.array.levels)
@@ -70,6 +68,37 @@ class GameFragment : Fragment() {
         val dialog = PokemonImageHintDialog(showsPokemonType)
         dialog.show(parentFragmentManager, "Pokemon Image Hint")
         view?.findViewById<Button>(R.id.hint_button)?.visibility = View.GONE
+    }
+
+    private fun addWordleLetterViews(wordlePlay: PokeWordle) {
+        for ((index, _) in wordlePlay.solutionWord.withIndex()) {
+            binding.apply {
+                wordleRow1.addView(createTextView(), index)
+                wordleRow2.addView(createTextView(), index)
+                wordleRow3.addView(createTextView(), index)
+                wordleRow4.addView(createTextView(), index)
+                wordleRow5.addView(createTextView(), index)
+                wordleRow6.addView(createTextView(), index)
+            }
+        }
+    }
+
+    private fun createTextView(): TextView {
+        val tv = TextView(context)
+        tv.gravity = Gravity.CENTER
+        tv.textSize = 30F
+        tv.setTextColor(resources.getColor(R.color.black))
+        tv.setBackgroundColor(resources.getColor(R.color.white))
+        tv.typeface = Typeface.DEFAULT_BOLD
+
+        tv.setPadding(5, 5, 5, 5)
+        val params = LinearLayout.LayoutParams(
+            100, 120
+        )
+        params.setMargins(10,0, 10, 0)
+
+        tv.layoutParams = params
+        return tv
     }
 
 }
