e5846acc7006c0bb320f52c30a4b40ce5528e773
Tomás Aranda <tomas.aranda.95@gmail.com>
2022-06-28 19:50:01 -0300

    feat: add basic stats views

--- app/src/main/java/com/example/poke_wordle/ui/StatsFragment.kt
+++ app/src/main/java/com/example/poke_wordle/ui/StatsFragment.kt
@@ -5,16 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import com.example.poke_wordle.R
+import com.example.poke_wordle.data.db.AppDatabase
+import com.example.poke_wordle.databinding.FragmentStatsBinding
+import kotlinx.coroutines.launch
 
 class StatsFragment : Fragment() {
+    private lateinit var binding: FragmentStatsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_stats, container, false)
+    ): View {
+        binding = FragmentStatsBinding.inflate(inflater)
+
+        lifecycleScope.launch {
+            val wordleDao = AppDatabase.getInstance(requireContext()).pokeWordlePlayDao()
+            binding.playCount.text = wordleDao.getWordlePlayCount().toString()
+            binding.winPercentage.text = wordleDao.getWordlePlayWinPercentage().toString()
+            binding.attemptsPercentage.text = wordleDao.getWordleAttemptsPercentages().toString()
+
+        }
+        return binding.root
     }
 
 }
