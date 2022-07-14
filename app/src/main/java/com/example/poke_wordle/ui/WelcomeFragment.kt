4089ffb0b7e8bb47db17fdbaabcbac641ace6ecf
Tomás Aranda <tomas.aranda.95@gmail.com>
2022-07-14 09:48:14 -0300

    refactor: add view binding to WelcomeFragment

--- app/src/main/java/com/example/poke_wordle/ui/WelcomeFragment.kt
+++ app/src/main/java/com/example/poke_wordle/ui/WelcomeFragment.kt
@@ -5,34 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.navigation.fragment.findNavController
 import com.example.poke_wordle.R
+import com.example.poke_wordle.databinding.FragmentWelcomeBinding
 import com.example.poke_wordle.ui.viewmodel.PokeWordleViewModel
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
 class WelcomeFragment : Fragment() {
     private val wordleViewModel by sharedViewModel<PokeWordleViewModel>()
+    private lateinit var binding: FragmentWelcomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         super.onCreate(savedInstanceState)
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
-    }
+        binding = FragmentWelcomeBinding.inflate(inflater)
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        val playButton = view.findViewById<Button>(R.id.play_button)
         wordleViewModel.wordle.observe(viewLifecycleOwner) { wordle ->
             if (wordle != null) {
-                playButton.setOnClickListener {
+                binding.playButton.setOnClickListener {
                     findNavController().navigate(R.id.action_welcomeFragment_to_gameFragment)
                 }
             } else {
-                playButton.setOnClickListener {
+                binding.playButton.setOnClickListener {
                     showLevelPickerDialog {
                         findNavController().navigate(R.id.action_welcomeFragment_to_gameFragment)
                     }
@@ -40,12 +36,18 @@ class WelcomeFragment : Fragment() {
             }
         }
 
-        view.findViewById<Button>(R.id.stats_button).setOnClickListener {
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.statsButton.setOnClickListener {
             val action = WelcomeFragmentDirections.actionWelcomeFragmentToStatsFragment()
             findNavController().navigate(action)
         }
 
-        view.findViewById<Button>(R.id.help_button).setOnClickListener {
+        binding.helpButton.setOnClickListener {
             val action = WelcomeFragmentDirections.actionWelcomeFragmentToHelpFragment()
             findNavController().navigate(action)
         }
